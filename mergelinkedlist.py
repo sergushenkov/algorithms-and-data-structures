b1abd2f001ce59f71012590c02e1ea553e15e5d5
Nikolay Sergushenkov <sergushenkov@gmail.com>
2023-03-20 10:25:56 +0300

    add type annotation for vars

--- mergelinkedlist.py
+++ mergelinkedlist.py
@@ -17,11 +17,11 @@ def merge_linked_list(llist_1: LinkedList,
     '''
     if llist_1.len() != llist_2.len():
         return None
-    sum_llist = LinkedList()
-    node_1 = llist_1.head
-    node_2 = llist_2.head
-    while node_1 is not None:
-        node = Node(node_1.value + node_2.value)
+    sum_llist: LinkedList = LinkedList()
+    node_1: None | Node = llist_1.head
+    node_2: None | Node = llist_2.head
+    while node_1 is not None and node_2 is not None:
+        node: Node = Node(node_1.value + node_2.value)
         sum_llist.add_in_tail(node)
         node_1 = node_1.next
         node_2 = node_2.next
