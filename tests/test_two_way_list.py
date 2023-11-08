eb719232ec284888563fac5f90de3a6aa1efed28
sergushenkov <sergushenkov@gmail.com>
2023-11-08 23:40:14 +0300

    add two_way_list

---
+++ tests/test_two_way_list.py
@@ -0,0 +1,257 @@
+from two_way_list import LinkedList, TwoWayList
+
+
+def test_class_vars():
+    assert LinkedList.RIGHT_OK == 1
+    assert LinkedList.RIGHT_ERR == 2
+    assert LinkedList.LEFT_OK == 1
+    assert LinkedList.LEFT_ERR == 2
+    assert LinkedList.FIND_R == 1
+    assert LinkedList.FIND_L == 2
+    assert LinkedList.FIND_ERR == 3
+    assert LinkedList.GET_OK == 1
+    assert LinkedList.GET_ERR == 2
+
+
+def test_init():
+    linked_list = LinkedList()
+    assert linked_list.size() == 0
+    assert linked_list.get_right_status() == LinkedList.RIGHT_OK
+    assert linked_list.get_left_status() == LinkedList.LEFT_OK
+    assert linked_list.get_find_status() == LinkedList.FIND_R
+    assert linked_list.get_get_status() == LinkedList.GET_OK
+
+
+def test_add_tail():
+    linked_list = LinkedList()
+    linked_list.add_tail(1)
+    value = linked_list.get()
+    assert value == 1
+    assert linked_list.get_get_status() == LinkedList.GET_OK
+    linked_list.add_tail(2)
+    value = linked_list.get()
+    assert value == 1
+    assert linked_list.get_get_status() == LinkedList.GET_OK
+    linked_list.add_tail(3)
+    value = linked_list.get()
+    assert value == 1
+    assert linked_list.get_get_status() == LinkedList.GET_OK
+
+
+def test_get():
+    linked_list = LinkedList()
+    value = linked_list.get()
+    assert value == 0
+    assert linked_list.get_get_status() == LinkedList.GET_ERR
+    linked_list.add_tail(0)
+    value = linked_list.get()
+    assert value == 0
+    assert linked_list.get_get_status() == LinkedList.GET_OK
+
+
+def test_size():
+    linked_list = LinkedList()
+    assert linked_list.size() == 0
+    linked_list.add_tail(1)
+    assert linked_list.size() == 1
+    linked_list.add_tail(2)
+    assert linked_list.size() == 2
+
+
+def test_is_head():
+    linked_list = LinkedList()
+    assert linked_list.is_head()
+    linked_list.add_tail(1)
+    assert linked_list.is_head()
+    linked_list.add_tail(2)
+    assert linked_list.is_head()
+    linked_list.right()
+    assert not linked_list.is_head()
+
+
+def test_right():
+    linked_list = LinkedList()
+    linked_list.right()
+    assert linked_list.get_right_status() == LinkedList.RIGHT_ERR
+    linked_list.add_tail(1)
+    linked_list.right()
+    assert linked_list.get_right_status() == LinkedList.RIGHT_ERR
+    value = linked_list.get()
+    assert value == 1
+    linked_list.add_tail(2)
+    linked_list.right()
+    assert linked_list.get_right_status() == LinkedList.RIGHT_OK
+    value = linked_list.get()
+    assert value == 2
+    linked_list.right()
+    assert linked_list.get_right_status() == LinkedList.RIGHT_ERR
+    value = linked_list.get()
+    assert value == 2
+
+
+def test_is_tail():
+    linked_list = LinkedList()
+    assert linked_list.is_tail()
+    linked_list.add_tail(1)
+    assert linked_list.is_tail()
+    linked_list.add_tail(2)
+    assert not linked_list.is_tail()
+    linked_list.right()
+    assert linked_list.is_tail()
+
+
+def test_is_value():
+    linked_list = LinkedList()
+    assert not linked_list.is_value()
+    linked_list.add_tail(1)
+    assert linked_list.is_value()
+
+
+def test_head():
+    linked_list = LinkedList()
+    linked_list.add_tail(1)
+    linked_list.add_tail(2)
+    linked_list.right()
+    linked_list.head()
+    assert linked_list.is_head()
+
+
+def test_tail():
+    linked_list = LinkedList()
+    linked_list.add_tail(1)
+    linked_list.add_tail(2)
+    linked_list.tail()
+    assert linked_list.is_tail()
+
+
+def test_put_right():
+    linked_list = LinkedList()
+    linked_list.put_right(1)
+    assert linked_list.size() == 1
+    assert linked_list.get() == 1
+    linked_list.put_right(2)
+    assert linked_list.size() == 2
+    assert linked_list.get() == 1
+    linked_list.right()
+    assert linked_list.is_tail()
+
+
+def test_put_left():
+    linked_list = LinkedList()
+    linked_list.put_left(1)
+    assert linked_list.size() == 1
+    assert linked_list.get() == 1
+    linked_list.put_left(2)
+    assert linked_list.size() == 2
+    assert linked_list.get() == 1
+
+
+def test_replace():
+    linked_list = LinkedList()
+    linked_list.replace(1)
+    assert linked_list.size() == 1
+    assert linked_list.get() == 1
+    linked_list.replace(2)
+    assert linked_list.size() == 1
+    assert linked_list.get() == 2
+
+
+def test_find():
+    linked_list = LinkedList()
+    linked_list.add_tail(1)
+    linked_list.add_tail(2)
+    linked_list.add_tail(1)
+    linked_list.add_tail(1)
+    linked_list.find(2)
+    assert linked_list.get_find_status() == LinkedList.FIND_R
+    assert linked_list.get() == 2
+    linked_list.find(2)
+    assert linked_list.get_find_status() == LinkedList.FIND_CUR
+    assert linked_list.get() == 2
+    linked_list.find(1)
+    assert linked_list.get_find_status() == LinkedList.FIND_R
+    assert linked_list.get() == 1
+    assert not linked_list.is_tail()
+    assert not linked_list.is_head()
+    linked_list.find(1)
+    assert linked_list.get_find_status() == LinkedList.FIND_R
+    assert linked_list.get() == 1
+    assert linked_list.is_tail()
+    linked_list.find(1)
+    assert linked_list.get_find_status() == LinkedList.FIND_L
+    assert linked_list.get() == 1
+    assert linked_list.is_head()
+    linked_list.find(3)
+    assert linked_list.get_find_status() == LinkedList.FIND_ERR
+    assert linked_list.get() == 1
+    assert linked_list.is_head()
+
+
+def test_remove():
+    linked_list = LinkedList()
+    linked_list.remove()
+    assert linked_list.size() == 0
+    linked_list.add_tail(1)
+    linked_list.remove()
+    assert linked_list.size() == 0
+    linked_list.add_tail(1)
+    linked_list.put_left(2)
+    linked_list.remove()
+    assert linked_list.size() == 1
+    assert linked_list.get() == 2
+    linked_list.add_tail(3)
+    linked_list.remove()
+    assert linked_list.size() == 1
+    assert linked_list.get() == 3
+    linked_list.put_left(2)
+    linked_list.add_tail(4)
+    linked_list.remove()
+    assert linked_list.size() == 2
+    assert linked_list.get() == 4
+
+
+def test_remove_all():
+    linked_list = LinkedList()
+    linked_list.add_tail(1)
+    linked_list.add_tail(2)
+    linked_list.add_tail(1)
+    linked_list.add_tail(1)
+    linked_list.remove_all(1)
+    assert linked_list.size() == 1
+    assert linked_list.get() == 2
+    linked_list.remove_all(2)
+    assert linked_list.size() == 0
+
+
+def test_clear():
+    linked_list = LinkedList()
+    linked_list.right()
+    linked_list.find(3)
+    linked_list.add_tail(1)
+    linked_list.add_tail(2)
+    linked_list.clear()
+    assert linked_list.size() == 0
+    assert linked_list.get_right_status() == LinkedList.RIGHT_OK
+    assert linked_list.get_left_status() == LinkedList.LEFT_OK
+    assert linked_list.get_find_status() == LinkedList.FIND_R
+    assert linked_list.get_get_status() == LinkedList.GET_OK
+
+
+def test_TwoWayList_left():
+    linked_list = TwoWayList()
+    linked_list.left()
+    assert linked_list.get_left_status() == LinkedList.LEFT_ERR
+    linked_list.add_tail(1)
+    linked_list.left()
+    assert linked_list.get_left_status() == LinkedList.LEFT_ERR
+    value = linked_list.get()
+    assert value == 1
+    linked_list.put_left(2)
+    linked_list.left()
+    assert linked_list.get_left_status() == LinkedList.LEFT_OK
+    value = linked_list.get()
+    assert value == 2
+    linked_list.left()
+    assert linked_list.get_left_status() == LinkedList.LEFT_ERR
+    value = linked_list.get()
+    assert value == 2
---
+++ two_way_list.py
@@ -0,0 +1,258 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class ParentLinkedList:
+    RIGHT_OK = 1  # последняя right() отработал нормально
+    RIGHT_ERR = 2  # справа от курсора нет элементов
+
+    LEFT_OK = 1  # последняя left() отработал нормально
+    LEFT_ERR = 2  # слева от курсора нет элементов
+
+    FIND_R = 1  # последний find() нашёл искомое значение справа
+    FIND_L = 2  # последний find() нашёл искомое значение слева
+    FIND_CUR = 3  # последний find() нашёл искомое значение только в курсоре
+    FIND_ERR = 4  # последний find() не нашёл искомое значение
+
+    GET_OK = 1  # последний get() отработал нормально
+    GET_ERR = 2  # список пуст
+
+    def __init__(self):
+        """
+        конструктор
+        постусловие:
+        1) создан новый пустой связанный список
+        2) курсор указывает на head
+        3) статусы операций не указывают на ошибки"""
+        self._head = None
+        self._tail = None
+        self._cursor = None
+        self._right_status = ParentLinkedList.RIGHT_OK
+        self._left_status = ParentLinkedList.LEFT_OK
+        self._find_status = ParentLinkedList.FIND_R
+        self._get_status = ParentLinkedList.GET_OK
+
+    # КОМАНДЫ
+
+    def head(self):
+        """
+        постусловие: курсор указывает на head"""
+        self._cursor = self._head
+
+    def tail(self):
+        """
+        постусловие: курсор указывает на tail"""
+        self._cursor = self._tail
+
+    def right(self):
+        """
+        предусловие: справа от курсора есть элементы
+        постусловие: курсор указывает на следующий узел за текущим"""
+        if self._cursor is None or self._cursor.next is None:
+            self._right_status = ParentLinkedList.RIGHT_ERR
+            return
+        self._right_status = ParentLinkedList.RIGHT_OK
+        self._cursor = self._cursor.next
+
+    def put_right(self, value):
+        """
+        постусловие:
+        1) справа от текущего узла вставлен новый узел с указанным значением
+        2) курсор указывает на текущий узел или на head() (если список был пуст)"""
+        node = Node(value)
+        if self._tail is None:
+            self._head = node
+            self._tail = node
+            self._cursor = node
+            return
+        node.prev = self._cursor
+        node.next = self._cursor.next
+        self._cursor.next = node
+        if node.next is None:
+            self._tail = node
+            return
+        node.next.prev = node
+
+    def put_left(self, value):
+        """
+        постусловие:
+        1) слева от текущего узла вставлен новый узел с указанным значением
+        2) курсор указывает на текущий узел или на head() (если список был пуст)"""
+        node = Node(value)
+        if self._tail is None:
+            self._head = node
+            self._tail = node
+            self._cursor = node
+            return
+        node.prev = self._cursor.prev
+        node.next = self._cursor
+        self._cursor.prev = node
+        if node.prev is None:
+            self._head = node
+            return
+        node.prev.next = node
+
+    def add_tail(self, value):
+        """
+        постусловие:
+        1) в конец списка добавлен новый узел с указанным значением
+        2) курсор указывает - на текущий узел, на head (если список был пуст)"""
+        node = Node(value)
+        if self._tail is None:
+            self._head = node
+            self._tail = node
+            self._cursor = node
+            return
+        node.prev = self._tail
+        self._tail.next = node
+        self._tail = node
+
+    def replace(self, value):
+        """
+        постусловие:
+        1) текущий элемент имеет заданное значение
+        2) курсор указывает - на текущий узел, на head (если список был пуст)"""
+        if self._tail is None:
+            node = Node(value)
+            self._head = node
+            self._tail = node
+            self._cursor = node
+            return
+        self._cursor.value = value
+
+    def find(self, value):
+        """
+        предусловие - в списке есть искомое значение
+        постусловие - курсор указывает (первый из имеющихся) - искомый элемент справа, искомый элемент слева, текущий узел"""
+        if self._tail is None:
+            self._find_status = ParentLinkedList.FIND_ERR
+            return
+        node = self._cursor
+        while node.next is not None:
+            node = node.next
+            if node.value == value:
+                self._cursor = node
+                self._find_status = ParentLinkedList.FIND_R
+                return
+        node = self._head
+        while node is not self._cursor:
+            if node.value == value:
+                self._cursor = node
+                self._find_status = ParentLinkedList.FIND_L
+                return
+            node = node.next
+        if node.value == value:
+            self._find_status = ParentLinkedList.FIND_CUR
+            return
+        self._find_status = ParentLinkedList.FIND_ERR
+
+    def remove(self):
+        """
+        постусловие:
+        1) текущий элемент удалён
+        2) курсор указывает (первый из имеющихся) - на правого соседа, на левого соседа, на head"""
+        if self._cursor is None:
+            return
+        if self._cursor is self._head and self._cursor is self._tail:
+            self._head = None
+            self._tail = None
+            self._cursor = None
+            return
+        if self._cursor is self._head:
+            self._head = self._head.next
+            self._head.prev = None
+            self._cursor = self._head
+            return
+        if self._cursor is self._tail:
+            self._tail = self._tail.prev
+            self._tail.next = None
+            self._cursor = self._tail
+            return
+        self._cursor.prev.next = self._cursor.next
+        self._cursor.next.prev = self._cursor.prev
+        self._cursor = self._cursor.next
+
+    def remove_all(self, value):
+        """
+        постусловие:
+        1) в списке нет заданных значений
+        2) курсор указывает (первый из имеющихся) - на текущий узел, на правого соседа, на левого соседа, на head"""
+        self.find(value)
+        while self.get_find_status() != ParentLinkedList.FIND_ERR:
+            self.remove()
+            self.find(value)
+
+    def clear(self):
+        """
+        постусловие:
+        1) список пуст
+        2) курсор указывает на head
+        3) статусы операций не указывают на ошибки"""
+        self._head = None
+        self._tail = None
+        self._cursor = None
+        self._right_status = ParentLinkedList.RIGHT_OK
+        self._left_status = ParentLinkedList.LEFT_OK
+        self._find_status = ParentLinkedList.FIND_R
+        self._get_status = ParentLinkedList.GET_OK
+
+    # ЗАПРОСЫ
+    def get(self):
+        """
+        предусловие - список не пуст"""
+        if self._cursor is None:
+            self._get_status = ParentLinkedList.GET_ERR
+            return 0
+        self._get_status = ParentLinkedList.GET_OK
+        return self._cursor.value
+
+    def size(self):
+        if self._head is None:
+            return 0
+        cnt = 1
+        node = self._head
+        while node.next is not None:
+            cnt += 1
+            node = node.next
+        return cnt
+
+    def is_head(self):
+        return self._cursor is self._head
+
+    def is_tail(self):
+        return self._cursor is self._tail
+
+    def is_value(self):
+        return self._cursor is not None
+
+    # ЗАПРОСЫ СТАТУСОВ
+    def get_right_status(self):
+        return self._right_status
+
+    def get_left_status(self):
+        return self._left_status
+
+    def get_find_status(self):
+        return self._find_status
+
+    def get_get_status(self):
+        return self._get_status
+
+
+class LinkedList(ParentLinkedList):
+    pass
+
+
+class TwoWayList(ParentLinkedList):
+    def left(self):
+        """
+        предусловие: слева от курсора есть элементы
+        постусловие: курсор указывает на узел за текущим"""
+        if self._cursor is None or self._cursor.prev is None:
+            self._left_status = ParentLinkedList.LEFT_ERR
+            return
+        self._left_status = ParentLinkedList.LEFT_OK
+        self._cursor = self._cursor.prev
